7760a079104da50ac1006a95d2cfe268c92684bc
PerumallaNaveenKumar <113343672+naveenperumallakumar@users.noreply.github.com>
2022-09-12 15:19:04 +0530

    Update streamlit_app.py
    Added emoji and with Avocado item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach &rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗Kale, Spinach &rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado')
